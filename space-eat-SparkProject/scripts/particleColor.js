7e62f20590271d4f64e7a67ba94dd5aaa42eb4a7
newyellow <newyellow2@gmail.com>
2020-03-16 05:49:02 +0800

    finish making textures adjustments and sound!

---
+++ space-eat-SparkProject/scripts/particleColor.js
@@ -0,0 +1,48 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene');
+const Animation = require('Animation');
+
+// Use export keyword to make a symbol available in scripting debug console
+
+// To use variables and functions across files, use export/import keyword
+// export const animationDuration = 10;
+
+// Use import keyword to import a symbol from another file
+// import { animationDuration } from './script.js'
+
+// To access scene objects
+// const directionalLight = Scene.root.find('directionalLight0');
+
+// To access class properties
+// const directionalLightIntensity = directionalLight.intensity;
+
+// To log messages to the console
+// Diagnostics.log('Console message logged from the script.');
+
+function SetupSmokeParticleFading () {
+    const alphaSampler = Animation.samplers.HSVA([
+            Animation.samplers.constant(1),
+            Animation.samplers.constant(1),
+            Animation.samplers.constant(1),
+            Animation.samplers.easeInCirc(1.0, 0.0)
+        ]);
+
+    var smokeParticle = Scene.root.find('smoke-particle');
+    smokeParticle.sizeModifier = Animation.samplers.easeInOutQuad(0.0, 0.2);
+    smokeParticle.hsvaColorModulationModifier = alphaSampler;
+}
+
+SetupSmokeParticleFading();
